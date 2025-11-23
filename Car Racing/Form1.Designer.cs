33e21ec64ca5213941f94e0108a9dc1aa71ff414
Eren Yusuf Can <canyusuferen@hotmail.com>
2025-11-23 13:37:56 +0300

    Proje dosyası ekle.

---
+++ Car Racing/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Car_Racing
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.crash = new System.Windows.Forms.PictureBox();
+            this.mycar = new System.Windows.Forms.PictureBox();
+            this.car2 = new System.Windows.Forms.PictureBox();
+            this.car1 = new System.Windows.Forms.PictureBox();
+            this.rode = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_score = new System.Windows.Forms.Label();
+            this.btn_startgame = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.crash)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mycar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.crash);
+            this.panel1.Controls.Add(this.mycar);
+            this.panel1.Controls.Add(this.car2);
+            this.panel1.Controls.Add(this.car1);
+            this.panel1.Controls.Add(this.rode);
+            this.panel1.Location = new System.Drawing.Point(21, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(507, 523);
+            this.panel1.TabIndex = 0;
+            // 
+            // crash
+            // 
+            this.crash.Image = ((System.Drawing.Image)(resources.GetObject("crash.Image")));
+            this.crash.Location = new System.Drawing.Point(259, 310);
+            this.crash.Name = "crash";
+            this.crash.Size = new System.Drawing.Size(100, 50);
+            this.crash.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.crash.TabIndex = 2;
+            this.crash.TabStop = false;
+            // 
+            // mycar
+            // 
+            this.mycar.Image = ((System.Drawing.Image)(resources.GetObject("mycar.Image")));
+            this.mycar.Location = new System.Drawing.Point(227, 354);
+            this.mycar.Name = "mycar";
+            this.mycar.Size = new System.Drawing.Size(69, 134);
+            this.mycar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mycar.TabIndex = 1;
+            this.mycar.TabStop = false;
+            // 
+            // car2
+            // 
+            this.car2.Image = ((System.Drawing.Image)(resources.GetObject("car2.Image")));
+            this.car2.Location = new System.Drawing.Point(355, 65);
+            this.car2.Name = "car2";
+            this.car2.Size = new System.Drawing.Size(69, 134);
+            this.car2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.car2.TabIndex = 1;
+            this.car2.TabStop = false;
+            // 
+            // car1
+            // 
+            this.car1.Image = ((System.Drawing.Image)(resources.GetObject("car1.Image")));
+            this.car1.Location = new System.Drawing.Point(104, 65);
+            this.car1.Name = "car1";
+            this.car1.Size = new System.Drawing.Size(69, 134);
+            this.car1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.car1.TabIndex = 1;
+            this.car1.TabStop = false;
+            // 
+            // rode
+            // 
+            this.rode.Image = ((System.Drawing.Image)(resources.GetObject("rode.Image")));
+            this.rode.Location = new System.Drawing.Point(0, 39);
+            this.rode.Name = "rode";
+            this.rode.Size = new System.Drawing.Size(513, 775);
+            this.rode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.rode.TabIndex = 0;
+            this.rode.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(208, 536);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Score:";
+            // 
+            // lbl_score
+            // 
+            this.lbl_score.AutoSize = true;
+            this.lbl_score.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_score.Location = new System.Drawing.Point(306, 536);
+            this.lbl_score.Name = "lbl_score";
+            this.lbl_score.Size = new System.Drawing.Size(25, 27);
+            this.lbl_score.TabIndex = 1;
+            this.lbl_score.Text = "0";
+            // 
+            // btn_startgame
+            // 
+            this.btn_startgame.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_startgame.Location = new System.Drawing.Point(82, 582);
+            this.btn_startgame.Name = "btn_startgame";
+            this.btn_startgame.Size = new System.Drawing.Size(399, 83);
+            this.btn_startgame.TabIndex = 2;
+            this.btn_startgame.Text = "Start Game";
+            this.btn_startgame.UseVisualStyleBackColor = true;
+            this.btn_startgame.Click += new System.EventHandler(this.btn_startgame_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 677);
+            this.Controls.Add(this.btn_startgame);
+            this.Controls.Add(this.lbl_score);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Car Racing";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.crash)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mycar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox rode;
+        private System.Windows.Forms.PictureBox mycar;
+        private System.Windows.Forms.PictureBox car2;
+        private System.Windows.Forms.PictureBox car1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_score;
+        private System.Windows.Forms.Button btn_startgame;
+        private System.Windows.Forms.PictureBox crash;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Car Racing/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Car_Racing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int score = 0;
+        int rodevelocity = 8;
+        int mycarvelocity = 5;
+
+        bool leftway = false;
+        bool rightway = false;
+
+        int carsvelocity = 10;
+
+        Random rnd = new Random();
+
+
+        public void startgame()
+        {
+
+            timer1.Interval = 16; // Yaklaşık 60 FPS
+
+            btn_startgame.Enabled = false;
+            crash.Visible = false;
+
+             mycarvelocity = 8;
+             carsvelocity = 10;
+             score = 0;
+
+            mycar.Left = 160;
+            mycar.Top = 300;
+
+            car1.Left = 30;
+            car1.Top = 50;
+
+            car2.Left = 320;
+            car2.Top = 50;
+
+            leftway = false;
+            rightway = false;
+
+            crash.Top = 294;
+            crash.Left = 165;
+
+            timer1.Start();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            startgame();
+            
+        }
+
+       
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            score++;
+            lbl_score.Text = score.ToString();
+
+            rode.Top += rodevelocity;
+            if (rode.Top > 400)
+            {
+                rode.Top = -100;
+            }
+
+            if (leftway)
+            {
+                mycar.Left -= mycarvelocity; 
+            }
+
+            if (rightway)
+            {
+                mycar.Left += mycarvelocity;
+            }
+
+            if (mycar.Left < 1) { leftway = false; }
+            else if (mycar.Left + mycar.Width > 510) { rightway = false; }
+
+            car1.Top += carsvelocity;
+            car2.Top += carsvelocity;
+
+            if(car1.Top>panel1.Height)
+            {
+                changethecar1();
+                car1.Left = rnd.Next(70,300);
+                car1.Top=rnd.Next(40,140) * -1;
+            }
+
+            if (car2.Top > panel1.Height)
+            {
+                changethecar2();
+                car2.Left = rnd.Next(70, 300);
+                car2.Top = rnd.Next(40, 140) * -1;
+            }
+
+            if(mycar.Bounds.IntersectsWith(car1.Bounds) || mycar.Bounds.IntersectsWith(car2.Bounds))
+             {
+                gameover();
+             }
+        }
+
+        private void changethecar1()
+        {
+            int sıra = rnd.Next(1, 7);
+            switch (sıra)
+            {
+                case 1: car1.Image = Properties.Resources.araba9; break;
+                case 2: car1.Image = Properties.Resources.araba2; break;
+                case 3: car1.Image = Properties.Resources.araba3; break;
+                case 4: car1.Image = Properties.Resources.araba4; break;
+                case 5: car1.Image = Properties.Resources.araba6; break;
+                case 6: car1.Image = Properties.Resources.araba7; break;
+                case 7: car1.Image = Properties.Resources.araba8; break;
+            }
+        }
+
+        private void changethecar2()
+        {
+            int sıra = rnd.Next(1, 7);
+            switch (sıra)
+            {
+                case 1:car2.Image = Properties.Resources.araba9;break;
+                case 2:car2.Image= Properties.Resources.araba2; break;
+                case 3:car2.Image=Properties.Resources.araba3; break;
+                case 4: car2.Image = Properties.Resources.araba4; break;
+                case 5: car2.Image = Properties.Resources.araba6; break;
+                case 6: car2.Image = Properties.Resources.araba7; break;
+                case 7: car2.Image = Properties.Resources.araba8; break;
+            }
+        }
+
+        private void gameover()
+        {
+            timer1.Stop();
+            btn_startgame.Enabled =true;
+            crash.Visible = true;
+            mycar.Controls.Add(crash);
+            crash.Location=new Point(7,-5);
+            crash.BringToFront();
+            crash.BackColor = Color.Transparent;
+            MessageBox.Show("Your score is: " + lbl_score.Text,"Bilgilendirme",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void btn_startgame_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left && mycar.Left>0) { leftway = true; }
+            if(e.KeyCode == Keys.Right && mycar.Right + mycar.Width<panel1.Width) { rightway = true; }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) {  leftway = false; }
+            if (e.KeyCode == Keys.Right) { rightway = false; }
+        }
+    }
+}
